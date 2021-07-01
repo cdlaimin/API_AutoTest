7bf185d526cd35d90765b346350555d854d61498
zhangjian <jian.zhang7@jiediankeji.com>
2021-07-01 16:26:19 +0800

    fix docker-compose not find

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ dir=$(cd `dirname $0`; pwd)
 echo $dir
 project_name=${dir##*/}
 echo ${project_name}
-names=( `docker-compose ps | awk '{print $1}' | grep ${project_name}` )
+names=( `docker-compose ps | awk '{print $7}' | grep ${project_name}` )
 echo "=================开始进行测试,当前测试HUB分支: $1================="
-echo $names
+echo ${names[0]}
 docker exec -i ${names[0]} pytest --show-capture=no -d --job_name="$2" --build_number="$3" --tx socket=${names[1]}:8888 --tx socket=${names[2]}:8888 --rsyncdir tests/ tests/ --alluredir=allure-results --clean-alluredir --reruns=1 || true
 echo "=================测试执行完成，一点点清理工作================="
