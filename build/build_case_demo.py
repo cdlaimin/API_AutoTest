d342ee03b064dfe5412a48cecce10ddf603f7bc2
zhangjian <jian.zhang7@jiediankeji.com>
2021-07-10 00:49:27 +0800

    add new case

--- build/build_case_demo.py
+++ build/build_case_demo.py
@@ -1,18 +1,23 @@
 from build.case import build_case
 
-path = 'tests/ts/interaction/test_publish_trends'
+path = 'tests/ts/interaction/test_share_trends'
 app = 'ts'
-api = 'thinksns/index.php?app=public&mod=Feed&act=PostFeed'
+api = 'thinksns/index.php?app=public&mod=Feed&act=shareFeed'
 method = 'post'
 headers = {
-    "Referer": "http://{ip}/index.php?app=public&mod=Index&act=index",
-    "Content-Type": "application/x-www-form-urlencoded"}
+    "Referer": "http://{ip}/thinksns/index.php?app=public",
+    "Content-Type": "application/x-www-form-urlencoded"
+}
 fixtures = ['params', ]
 data = [
     {
         "body": "$$sentence",
-        "type": "post",
-        "app_name": "public"
+        "type": "feed",
+        "app_name": "public",
+        "sid": "@@feed_id",
+        "comment": 1,
+        "curid": "@@feed_id",
+        "curtable": "feed"
     },
 ]
 expect = [
@@ -24,8 +29,8 @@ info_list = [
     {
         'model': 'ThinkSNS',
         'func': '朋友圈',
-        'case_name': '发布动态',
-        'description': '在朋友圈发布一条动态。预期成功。',
+        'case_name': '转发动态',
+        'description': '在朋友圈转发一条动态。预期成功。',
         'level': 'M'
     },
 ]
---
+++ tests/ts/interaction/test_share_trends/test_share_trends.py
@@ -0,0 +1,16 @@
+import pytest
+
+from utils.tools.request import Requests
+from utils.test.verify import verification
+
+
+def test_share_trends(params):
+    expect = params.pop('expect')
+    response = Requests(**params).requests()
+
+    # 验证结果
+    verification(expect, response)
+
+
+if __name__ == '__main__':
+    pytest.main(['-s'])
