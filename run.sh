855f94b2fd73f55197246e9be368ff53266d9ea3
zhangjian <jian.zhang7@jiediankeji.com>
2021-12-31 21:49:02 +0800

    optimize Jenkinsfile

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-dir=$(cd `dirname $0`; pwd)
+dir=$(
+  cd $(dirname "$0")
+  pwd
+)
 project_name=${dir##*/}
-echo ${project_name}
-names=( `docker-compose ps | grep ${project_name} | awk '{print $1}'` )
+names=($(docker ps | grep "${project_name}" | awk '{print $1}'))
 echo "=================开始进行测试,当前测试HUB分支: $2================="
 echo "${names[@]}"
 #docker exec -i ${names[0]} pytest -m "$5" --show-capture=no --job_name="$1" --build_number="$2" --target="$3" --send_wechat="$6" --wechat_token="$7" -d --tx socket=${names[1]}:8888 --tx socket=${names[2]}:8888 --rsyncdir tests/ tests/ --alluredir=allure-results --clean-alluredir --reruns=1 || true
 #docker exec -i ${names[0]} pytest -m "$5" --show-capture=no --job_name="$1" --build_number="$2" --target="$3" --send_wechat="$6" --wechat_token="$7" -d --tx socket=${names[1]}:8888 --rsyncdir tests/ tests/ --alluredir=allure-results --clean-alluredir --reruns=1 || true
 docker exec -i "${names[0]}" pytest --show-capture=no --job_name="$1" --build_number="$3" --server="$4" --env="$5" --send_wechat="$6" --wechat_token="$7" -d --tx socket="${names[1]}":8888 --tx socket="${names[2]}":8888 --rsyncdir tests/ tests/ --alluredir=allure-results --clean-alluredir --reruns=1 || true
-
