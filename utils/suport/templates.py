a239c1ce8e22018e7df940344b02210bff8e8c57
zhangjian <jian.zhang7@jiediankeji.com>
2022-01-01 16:31:28 +0800

    optimize report

--- utils/suport/templates.py
+++ utils/suport/templates.py
@@ -3,23 +3,24 @@
 """
 
 STANDARD_RUN = '''def run({self}, {role}, {api_list}):
+        # 向报告中添加二级分类信息
+        allure.dynamic.story({self}.__doc__ or {self}.__class__.__name__)
+        
+        # 遍历接口，开始测试
         for api in {api_list}:
-            # 向报告中添加二级分类信息
-            allure.dynamic.story({self}.__doc__ or {self}.__class__.__name__)
-    
             # 向报告中添加测试步骤信息
-            # with allure.step(api[0]):
-            # 先关联数据
-            api_name, api_data = relate(api, {self})
-
-            expect = api_data.pop('expect')
-            response = call_api({role}, api_data)
-
-            # 验证结果
-            verify(expect, response)
-
-            # 保存响应json
-            self.__setattr__(api_name, response.json())'''
+            with allure.step(api[0]):
+                # 先关联数据
+                api_name, api_data = relate(api, {self})
+    
+                expect = api_data.pop('expect')
+                response = call_api({role}, api_data)
+    
+                # 验证结果
+                verify(expect, response)
+    
+                # 保存响应json
+                self.__setattr__(api_name, response.json())'''
 
 REPORT = \
     '''### {title} [点击查看报告]({url})
