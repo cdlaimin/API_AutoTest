69da38a60d6cee1cb9ac54360d93233fc8943cab
zhangjian <jian.zhang7@jiediankeji.com>
2022-01-01 15:32:45 +0800

    optimize report

--- utils/suport/templates.py
+++ utils/suport/templates.py
@@ -2,31 +2,33 @@
 框架使用到的模板
 """
 
-STANDARD_RUN = '''def run({self}, {role}, {api_list}):
-    for api in {api_list}:
-        # 向报告中添加二级分类信息
-        allure.dynamic.story({self}.__doc__ or {self}.__class__.__name__)
+STANDARD_RUN = \
+    '''def run({self}, {role}, {api_list}):
+        for api in {api_list}:
+            # 向报告中添加二级分类信息
+            allure.dynamic.story({self}.__doc__ or {self}.__class__.__name__)
+    
+            # 向报告中添加测试步骤信息
+            with allure.step(api[0]):
+                # 先关联数据
+                api_name, api_data = relate(api, {self})
+    
+                expect = api_data.pop('expect')
+                response = call_api({role}, api_data)
+    
+                # 验证结果
+                verify(expect, response)
+    
+                # 保存响应json
+                self.__setattr__(api_name, response.json())'''
 
-        # 向报告中添加测试步骤信息
-        with allure.step(api[0]):
-            # 先关联数据
-            api_name, api_data = relate(api, {self})
-
-            expect = api_data.pop('expect')
-            response = call_api({role}, api_data)
-
-            # 验证结果
-            verify(expect, response)
-
-            # 保存响应json
-            self.__setattr__(api_name, response.json())'''
-
-REPORT = '''### {title} [点击查看报告]({url})\n\n\
+REPORT = \
+'''### {title} [点击查看报告]({url})\n\n\
 
 #### 功能模块
 > 1、用例管理\n\
 > 2、任务管理\n\
-\n\
+## \n\
 测试结果: **{result}**\n\
 通过率: **{pass_rate}**\n\
 测试耗时: **{h}小时 {m}分 {s}秒**\n\n\
